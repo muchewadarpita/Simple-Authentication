212048f0510ad8e457fce0757a5c8f2dfd2081d0
Arpita Balaji Muchewad <96560427+muchewadarpita@users.noreply.github.com>
2023-03-29 13:07:41 +0530

    Initial commit

---
+++ src/main/java/com/example/SimpleAuthentication/SimpleAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimpleAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAuthenticationApplication.class, args);
+	}
+
+}
